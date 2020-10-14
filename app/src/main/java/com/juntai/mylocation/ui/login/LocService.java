54e636ea1d2e1a0a22a393d458a78f08e57c7b64
宁 李 <pcl5389@163.com>
2020-10-14 17:26:18 +0800

    整理代码前台与后台服务分离

--- app/src/main/java/com/juntai/mylocation/ui/login/LocService.java
+++ app/src/main/java/com/juntai/mylocation/ui/login/LocService.java
@@ -1,17 +1,39 @@
 package com.juntai.mylocation.ui.login;
 
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
 import android.app.Service;
+import android.content.Context;
 import android.content.Intent;
+import android.graphics.Color;
+import android.os.Environment;
 import android.os.IBinder;
+import android.os.Looper;
 
+import com.juntai.mylocation.R;
+import com.tencent.map.geolocation.TencentLocation;
+import com.tencent.map.geolocation.TencentLocationListener;
+import com.tencent.map.geolocation.TencentLocationManager;
+import com.tencent.map.geolocation.TencentLocationRequest;
+
+import java.io.File;
 import java.io.IOException;
 import java.io.OutputStream;
+import java.io.RandomAccessFile;
+import java.lang.ref.WeakReference;
+import java.math.BigDecimal;
 import java.net.Socket;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.Timer;
 import java.util.TimerTask;
 
+import static com.tencent.map.geolocation.TencentLocationRequest.REQUEST_LEVEL_GEO;
+
 public class LocService extends Service {
     /** 标识服务如果被杀死之后的行为 */
+    static final int LOCATION_RATE=120000;
     int mStartMode = START_STICKY;
     static boolean bConnected=false;
     static Socket socket;
@@ -19,6 +41,21 @@ public class LocService extends Service {
     private static final int PORT = 9527;
     private static Timer timer=null;
     public static  String mac="";
+    static String last_loc_time="";
+    static String _last_loc_time="";
+    NotificationManager notificationManager=null;
+    boolean isCreateChannel=false;
+    String NOTIFICATION_CHANNEL_NAME="获取位置";
+
+    static BigDecimal lat= BigDecimal.valueOf(0);
+    static BigDecimal lng= BigDecimal.valueOf(0);
+    static String addr="";
+    static String area="";
+
+    static TencentLocationManager mLocationManager=null;
+    static InnerLocationListener mLocationListener;
+    static TencentLocationRequest request;
+
     /** 绑定的客户端接口 */
     IBinder mBinder;
     /** 标识是否可以使用onRebind */
@@ -26,16 +63,66 @@ public class LocService extends Service {
     /** 当服务被创建时调用. */
     @Override
     public void onCreate() {
+        super.onCreate();
         //连接服务器
-        Send("Hello!"+ mac);
+        if(mLocationManager==null) {
+            mLocationManager = TencentLocationManager.getInstance(this);
+            mLocationListener = new InnerLocationListener();
+            request = TencentLocationRequest.create();
+            request.setAllowCache(false);
+            request.setInterval(LOCATION_RATE / 2);
+            request.setAllowGPS(true);
+            request.setRequestLevel(REQUEST_LEVEL_GEO);
+            request.setAllowDirection(false);
+
+            mLocationManager.enableForegroundLocation(2020, buildNotification());
+            mLocationManager.requestLocationUpdates(request, mLocationListener, Looper.getMainLooper());
+
+            Send("Hello!" + mac);
+        }
         //启动定时器
         if(timer==null)
         {
             start_timer();
         }
-        super.onCreate();
     }
 
+    private Notification buildNotification() {
+        Notification.Builder builder = null;
+        Notification notification = null;
+        if (android.os.Build.VERSION.SDK_INT >= 26) {
+            //Android O上对Notification进行了修改，如果设置的targetSDKVersion>=26建议使用此种方式创建通知栏
+            if (notificationManager == null) {
+                notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+            }
+            String channelId = getPackageName();
+            if (!isCreateChannel) {
+                NotificationChannel notificationChannel = new NotificationChannel(channelId,
+                        NOTIFICATION_CHANNEL_NAME, NotificationManager.IMPORTANCE_DEFAULT);
+                notificationChannel.enableLights(true);//是否在桌面icon右上角展示小圆点
+                notificationChannel.setLightColor(Color.BLUE); //小圆点颜色
+                notificationChannel.setShowBadge(true); //是否在久按桌面图标时显示此渠道的通知
+                notificationManager.createNotificationChannel(notificationChannel);
+                isCreateChannel = true;
+            }
+            builder = new Notification.Builder(getApplicationContext(), channelId);
+        } else {
+            builder = new Notification.Builder(getApplicationContext());
+        }
+
+        builder.setSmallIcon(R.mipmap.ic_launcher_small)
+                .setContentTitle("定位中")
+                .setContentText("正在定位")
+                //.setLargeIcon(BitmapFactory.decodeResource(getResources(), R.mipmap.ic_launcher))
+                .setWhen(System.currentTimeMillis());
+
+        if (android.os.Build.VERSION.SDK_INT >= 16) {
+            notification = builder.build();
+        } else {
+            notification = builder.getNotification();
+        }
+        return notification;
+    }
 
     public static void Send(String content)
     {
@@ -79,14 +166,10 @@ public class LocService extends Service {
         timer = new Timer();
         timer.scheduleAtFixedRate(new TimerTask() {
             public void run() {
-                //Date day=new Date();
-                //SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-                //System.out.println(df.format(day));
-                //Send(df.format(day));
-                LoginActivity.send_location();
-                //System.err.println("-------延迟5000毫秒，每1000毫秒执行一次--------"+df.format(day));
+                writeLogs("请求定位");
+                send_location();
             }
-        }, 1000, 120000);
+        }, 1000, LOCATION_RATE);
     }
 
     /** 调用startService()启动服务时回调 */
@@ -111,13 +194,10 @@ public class LocService extends Service {
     /** 通过bindService()将客户端绑定到服务时调用*/
     @Override
     public void onRebind(Intent intent) {
-
     }
-
     /** 服务不再有用且将要被销毁时调用 */
     @Override
     public void onDestroy() {
-        System.err.println("连接已断开！");
         //断开服务器连接
         if(bConnected)
         {
@@ -125,8 +205,9 @@ public class LocService extends Service {
             {
                 socket.close();
             }
-           catch (Exception e)
-           {}
+           catch (Exception e){}
+
+            mLocationManager=null;
             bConnected=false;
         }
         //关闭计时器
@@ -136,12 +217,71 @@ public class LocService extends Service {
             timer=null;
         }
     }
-
     public static class MyRunnable implements Runnable {
         String Content="";
         public MyRunnable(String parameter) {
             Content=parameter;
         }
         public void run() {}
+    }
+
+    public static void writeLogs(String str) {
+        String filePath = null;
+        boolean hasSDCard = Environment.getExternalStorageState().equals(Environment.MEDIA_MOUNTED);
+        if (hasSDCard) {
+            filePath =Environment.getExternalStorageDirectory().toString() + File.separator +"hello.txt";
+        } else
+            filePath =Environment.getDownloadCacheDirectory().toString() + File.separator +"hello.txt";
+        try {
+            File file = new File(filePath);
+            if (!file.exists()) {
+                File dir = new File(file.getParent());
+                dir.mkdirs();
+                file.createNewFile();
+            }
+            RandomAccessFile raf = new RandomAccessFile(file, "rwd");
+            raf.seek(file.length());
+
+            Date day=new Date();
+            SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+            raf.write((df.format(day)+":"+str + "\r\n").getBytes());
+            raf.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+    public static void send_location()
+    {
+        if(_last_loc_time!=last_loc_time)
+        {
+            _last_loc_time=last_loc_time;
+            String my_location=String.format("%s|%s|%s|%s|%s", lat, lng, addr, area, last_loc_time);
+            LocService.Send(my_location);
+        }
+    }
+
+    public static class InnerLocationListener implements TencentLocationListener {
+        private WeakReference<LoginActivity> mMainActivityWRF;
+        public InnerLocationListener() {}
+        @Override
+        public void onLocationChanged(TencentLocation _loc, int error, String reason) {
+            last_loc_time=(new SimpleDateFormat("yyyy-MM-dd HH:mm:ss")).format(new Date());
+            if(lat!=BigDecimal.valueOf(_loc.getLatitude()) || lng!=BigDecimal.valueOf(_loc.getLongitude()))
+            {
+                lat=BigDecimal.valueOf(_loc.getLatitude());
+                lng=BigDecimal.valueOf(_loc.getLongitude());
+                addr=_loc.getAddress();
+                area=_loc.getName();
+                writeLogs("->定位成功！"+ String.format("%s|%s|%s|%s", lat, lng, addr, area));
+            }
+            else
+            {
+                writeLogs("->定位成功！位置同上");
+            }
+        }
+        @Override
+        public void onStatusUpdate(String name, int status, String desc) {
+            writeLogs("StatusUpdate:"+ "name: " + name + "status: " + status + "desc: " + desc);
+        }
     }
 }
--- app/src/main/java/com/juntai/mylocation/ui/login/LoginActivity.java
+++ app/src/main/java/com/juntai/mylocation/ui/login/LoginActivity.java
@@ -1,53 +1,29 @@
 package com.juntai.mylocation.ui.login;
 
 import android.Manifest;
-import android.app.Notification;
-import android.app.NotificationChannel;
-import android.app.NotificationManager;
 import android.content.Context;
 import android.content.Intent;
-import android.graphics.Color;
+import android.content.SharedPreferences;
 import android.net.wifi.WifiInfo;
 import android.net.wifi.WifiManager;
 import android.os.Build;
 import android.os.Bundle;
-import android.os.Looper;
-import android.util.Log;
+import android.telephony.TelephonyManager;
+import android.text.InputType;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.ProgressBar;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import com.juntai.mylocation.R;
-import com.tencent.map.geolocation.TencentLocation;
-import com.tencent.map.geolocation.TencentLocationListener;
-import com.tencent.map.geolocation.TencentLocationManager;
-import com.tencent.map.geolocation.TencentLocationRequest;
-
-import java.lang.ref.WeakReference;
+import java.util.UUID;
 
 import pub.devrel.easypermissions.AfterPermissionGranted;
 import pub.devrel.easypermissions.EasyPermissions;
 
-import static com.tencent.map.geolocation.TencentLocationRequest.REQUEST_LEVEL_GEO;
-
 public class LoginActivity extends AppCompatActivity {
-    private static final int LOC_NOTIFICATIONID = 2020;
-    static TencentLocationManager mLocationManager;
-    static InnerLocationListener mLocationListener;
-    static TencentLocationRequest request;
-    NotificationManager notificationManager=null;
-    public static String mac="";
-    String TAG="Login";
-
-    static double lat=0;
-    static double lng=0;
-    static String addr="";
-    static String area="";
-
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -56,83 +32,35 @@ public class LoginActivity extends AppCompatActivity {
         if (Build.VERSION.SDK_INT >= 23) {
             requirePermission();
         }
-        mac= LocService.mac=getMacAddress();
-
-        mLocationManager= TencentLocationManager.getInstance(this);
-        mLocationListener = new InnerLocationListener(new WeakReference<LoginActivity>(this));
-        request= TencentLocationRequest.create();
-        request.setAllowGPS(true);
-        request.setRequestLevel(REQUEST_LEVEL_GEO);
-        request.setAllowDirection(false);
-        mLocationManager.enableForegroundLocation(LOC_NOTIFICATIONID, buildNotification());
-
-        Intent intentOne = new Intent(this, LocService.class);
-        intentOne.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-        startService(intentOne);
-
+        String mac= LocService.mac=getPhoneSign();
         final EditText usernameEditText = findViewById(R.id.username);
         usernameEditText.setText(mac);
+        usernameEditText.setInputType(InputType.TYPE_NULL);
         final EditText passwordEditText = findViewById(R.id.password);
-        final Button loginButton = findViewById(R.id.login);
-        final ProgressBar loadingProgressBar = findViewById(R.id.loading);
+        passwordEditText.setInputType(InputType.TYPE_NULL);
 
+        final Button loginButton = findViewById(R.id.login);
         loginButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 if (Build.VERSION.SDK_INT >= 23) {
                     requirePermission();
                 }
-                loadingProgressBar.setVisibility(View.VISIBLE);
-                mLocationManager.requestSingleFreshLocation(request, mLocationListener, Looper.getMainLooper());
-
-                String my_location=String.format("%s|%s|%s|%s", lat, lng, addr, area);
+                String my_location=String.format("%s|%s|%s|%s", LocService.lat, LocService.lng, LocService.addr, LocService.area);
                 Toast.makeText(getApplicationContext(), my_location, Toast.LENGTH_LONG).show();
-                loadingProgressBar.setVisibility(View.INVISIBLE);
             }
         });
-    }
-    boolean isCreateChannel=false;
-    String NOTIFICATION_CHANNEL_NAME="获取位置";
 
-    private Notification buildNotification() {
-        Notification.Builder builder = null;
-        Notification notification = null;
-        if (android.os.Build.VERSION.SDK_INT >= 26) {
-            //Android O上对Notification进行了修改，如果设置的targetSDKVersion>=26建议使用此种方式创建通知栏
-            if (notificationManager == null) {
-                notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        passwordEditText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                passwordEditText.setText(LocService.last_loc_time);
             }
-            String channelId = getPackageName();
-            if (!isCreateChannel) {NotificationChannel notificationChannel = new NotificationChannel(channelId,
-                    NOTIFICATION_CHANNEL_NAME, NotificationManager.IMPORTANCE_DEFAULT);
-                notificationChannel.enableLights(true);//是否在桌面icon右上角展示小圆点
-                notificationChannel.setLightColor(Color.BLUE); //小圆点颜色
-                notificationChannel.setShowBadge(true); //是否在久按桌面图标时显示此渠道的通知
-                notificationManager.createNotificationChannel(notificationChannel);
-                isCreateChannel = true;
-            }
-            builder = new Notification.Builder(getApplicationContext(), channelId);
-        } else {
-            builder = new Notification.Builder(getApplicationContext());
-        }
+        });
 
-        builder.setSmallIcon(R.mipmap.ic_launcher_small)
-                .setContentTitle("定位中")
-                .setContentText("正在定位");
-                //.setLargeIcon(BitmapFactory.decodeResource(getResources(), R.mipmap.ic_launcher))
-                //.setWhen(System.currentTimeMillis());
-
-        if (android.os.Build.VERSION.SDK_INT >= 16) {
-            notification = builder.build();
-        } else {
-            notification = builder.getNotification();
-        }
-        return notification;
-    }
-
-    public static void send_location()
-    {
-        mLocationManager.requestLocationUpdates(request, mLocationListener, Looper.getMainLooper());
+        Intent intentOne = new Intent(this, LocService.class);
+        intentOne.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        startService(intentOne);
     }
 
     @AfterPermissionGranted(1)
@@ -158,37 +86,9 @@ public class LoginActivity extends AppCompatActivity {
                     1, Build.VERSION.SDK_INT >= 29 ? permissionsForQ : permissions);
         }
     }
-
-    private static class InnerLocationListener implements TencentLocationListener {
-        private WeakReference<LoginActivity> mMainActivityWRF;
-        public InnerLocationListener(WeakReference<LoginActivity> mainActivityWRF) {
-            mMainActivityWRF = mainActivityWRF;
-        }
-        @Override
-        public void onLocationChanged(TencentLocation _loc, int error, String reason) {
-            if(_loc.getLatitude()>1)
-                mLocationManager.removeUpdates(mLocationListener);
-            if(lat!=_loc.getLatitude() || lng!=_loc.getLongitude())
-            {
-                String my_location=String.format("%s|%s|%s|%s",
-                        lat=_loc.getLatitude(),
-                        lng=_loc.getLongitude(),
-                        addr=_loc.getAddress(),
-                        area=_loc.getName());
-                LocService.Send(my_location);
-            }
-            else
-                LocService.Send("==");
-        }
-        @Override
-        public void onStatusUpdate(String name, int status, String desc) {
-            Log.i("Log", "name: " + name + "status: " + status + "desc: " + desc);
-        }
-    }
     public String getMacAddress() {
         String macAddress = null;
-        WifiManager wifiManager =
-                (WifiManager)getApplicationContext().getSystemService(Context.WIFI_SERVICE);
+        WifiManager wifiManager = (WifiManager)getApplicationContext().getSystemService(Context.WIFI_SERVICE);
         WifiInfo info = ( null == wifiManager ? null : wifiManager.getConnectionInfo());
         if (!wifiManager.isWifiEnabled())
         {
@@ -201,6 +101,66 @@ public class LoginActivity extends AppCompatActivity {
         }
         return macAddress;
     }
+    //获取手机的唯一标识
+    public String getPhoneSign(){
+        StringBuilder deviceId = new StringBuilder();
+        // 渠道标志
+        //deviceId.append("a");
+        try {
+            //IMEI（imei）
+            TelephonyManager tm = (TelephonyManager) this.getSystemService(Context.TELEPHONY_SERVICE);
+            String imei = tm.getDeviceId();
+            if(!TextUtils.isEmpty(imei)){
+                deviceId.append("imei");
+                deviceId.append(imei);
+                return deviceId.toString();
+            }
+            //序列号（sn）
+            String sn = tm.getSimSerialNumber();
+            if(!TextUtils.isEmpty(sn)){
+                deviceId.append("sn");
+                deviceId.append(sn);
+                return deviceId.toString();
+            }
+            //如果上面都没有， 则生成一个id：随机码
+            String mac = getMacAddress();
+            if(!TextUtils.isEmpty(mac)){
+                deviceId.append("mac");
+                deviceId.append(uuid);
+                return deviceId.toString();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            deviceId.append("id").append(getUUID());
+        }
+        return deviceId.toString();
+    }
+    /**
+     * 得到全局唯一UUID
+     */
+    private String uuid;
+    public String getUUID(){
+        SharedPreferences mShare = getSharedPreferences("uuid",MODE_PRIVATE);
+        if(mShare != null){
+            uuid = mShare.getString("uuid", "");
+        }
+        if(TextUtils.isEmpty(uuid)){
+            uuid = UUID.randomUUID().toString();
+            mShare.edit().putString("uuid",uuid).commit();
+        }
+        return uuid;
+    }
+
+
+
+
+
+
+
+
+
+
+
 }
 
 
